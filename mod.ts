d4c3526e4487dfc0d3160ef62402b7dd373e1d68
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2025-01-12 17:14:46 +0900

    chore: update

--- mod.ts
+++ mod.ts
@@ -1,10 +1,25 @@
 import * as fs from "@std/fs";
-import { BlobReader, type Entry, TextWriter, ZipReader } from "@zip-js/zip-js";
+import {
+  BlobReader,
+  BlobWriter,
+  type Entry,
+  TextWriter,
+  Uint8ArrayReader,
+  ZipReader,
+  ZipWriter,
+} from "@zip-js/zip-js";
+import * as path from "@std/path";
+import { walk } from "@std/fs/walk";
 
 export type AddonInfo = {
   id: string;
   name: string;
   version: string;
+};
+
+export type ExtInfo = {
+  type: "dir" | "xpi";
+  info: AddonInfo;
 };
 
 export class Result<T> {
@@ -33,6 +48,80 @@ export class Result<T> {
   public err(): Error | undefined {
     return this.error;
   }
+}
+
+function isValidAOMAddonId(s: string) {
+  return /^(\{[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\}|[a-z0-9-\._]*\@[a-z0-9-\._]+)$/i
+    .test(
+      s || "",
+    );
+}
+
+export function getID(manifest: AddonInfo): string | undefined {
+  if (manifest.id) {
+    return isValidAOMAddonId(manifest.id) ? manifest.id : undefined;
+  }
+
+  // This is currently used to keep the backward compatible behavior
+  // expected on the deprecated jetpack extensions manifest file.
+  if (manifest.name) {
+    const id = `@${manifest.name}`;
+    return isValidAOMAddonId(id) ? id : undefined;
+  }
+
+  return undefined;
+}
+
+export async function readExtInfo(
+  addonPath: string,
+  textDecoder?: TextDecoder,
+): Promise<Result<ExtInfo>> {
+  const isDir = (await Deno.stat(addonPath)).isDirectory;
+  let type = "file";
+  if (isDir) {
+    type = "dir";
+  }
+  const result = { type, info: {} };
+  const decoder = textDecoder || new TextDecoder();
+  let doc: string;
+  if (isDir) {
+    const json_path = path.join(addonPath, "manifest.json");
+    const data = Deno.readFileSync(json_path);
+    doc = decoder.decode(data);
+  } else {
+    const fileData = await Deno.readFile(addonPath);
+    const zipFileBlob: Blob = new Blob([fileData]);
+    const zipFileReader = new BlobReader(zipFileBlob);
+
+    const zipReader = new ZipReader(zipFileReader);
+    const entries: Entry[] = await zipReader.getEntries();
+    const jsonEntry: Entry | undefined = entries.find((entry) =>
+      entry.filename == "manifest.json"
+    );
+    if (!jsonEntry) {
+      return Result.from_error(new Error("do not contain manifest.json"));
+    }
+    const jsonWriter = new TextWriter();
+    doc = await jsonEntry.getData!(jsonWriter);
+    await zipReader.close();
+  }
+  const webExtManifest = JSON.parse(doc);
+  const details = {
+    id: "",
+    name: "",
+    version: "",
+  };
+  details.id = (
+    (webExtManifest.browser_specific_settings || {}).gecko || {}
+  ).id;
+  if (!details.id) {
+    details.id = ((webExtManifest.applications || {}).gecko || {})
+      .id as string;
+  }
+  details.name = webExtManifest.name;
+  details.version = webExtManifest.version;
+  result.info = details;
+  return Result.from_data(result as ExtInfo);
 }
 
 /// read the xpi file and get the data
@@ -102,4 +191,43 @@ export async function parseExtManifest(
   details.name = webExtManifest.name;
   details.version = webExtManifest.version;
   return Result.from_data(details);
+}
+
+export type XpiInfo = {
+  zipFileWriter: BlobWriter;
+  id: string;
+};
+
+export async function packageXpi(
+  sourceDir: string,
+  textDecoder?: TextDecoder,
+  targetFolder?: string,
+) {
+  const zipFileWriter: BlobWriter = new BlobWriter();
+
+  const zipWriter = new ZipWriter(zipFileWriter);
+  for await (const fileEntry of walk(sourceDir)) {
+    if (!fileEntry.isFile) {
+      continue;
+    }
+    const data = await Deno.readFile(fileEntry.path);
+    const dataReader = new Uint8ArrayReader(data);
+    const zippath = path.relative(sourceDir, fileEntry.path);
+    await zipWriter.add(zippath, dataReader);
+  }
+  zipWriter.close();
+  const info_result = await readExtInfo(sourceDir, textDecoder);
+  if (info_result.is_err()) {
+    return;
+  }
+  const info = info_result.data()!.info;
+  const fileNamePre = getID(info);
+  if (!fileNamePre) {
+    return;
+  }
+  const fileName = fileNamePre + ".xpi";
+  const target = path.join(targetFolder || Deno.cwd(), fileName);
+  const zipFileBlob: Blob = await zipFileWriter.getData();
+
+  await Deno.writeFile(target, zipFileBlob.stream());
 }
